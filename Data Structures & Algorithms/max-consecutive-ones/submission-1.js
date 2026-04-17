7198c0e221e2a8d4cd1485f539666c2e3562d11e
Danila <112901749+QuverOK@users.noreply.github.com>
2026-04-17 20:19:24 +0300

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMaxConsecutiveOnes(nums) {
+        let max = 0;
+        let counter = 0;
+        for (let i = 0; i <= nums.length; i++) {
+            if (nums[i] === 1) {
+                counter++;
+            } else {
+                if (max < counter) {
+                    max = counter;
+                }
+
+                counter = 0;
+            }
+        }
+
+        return max;
+    }
+}
